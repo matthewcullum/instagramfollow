f75d2793f4bdd270f0986f3a708c7997aae879c6
Matthew Cullum <creativelyconcise@gmail.com>
2015-03-01 16:48:51 -0700

    No longer displays private instagram profiles in search results. Shows follow count for each user in search results

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -3,13 +3,27 @@ class InstagramController < ApplicationController
 
   def search
     search_term = params[:search_term]
-    @search_results = @client.user_search search_term
+    @search = @client.user_search search_term, count: 8
+
+    @search_results = []
+    @search.each do |result|
+      begin
+        user = @client.user(result.id)
+        @search_results << user
+      rescue
+        nil
+      end
+    end
   end
 
   def view_profile
-    @profile = @client.user(params[:id])
-    @followed_by_count = @profile.counts.followed_by
-    #@profile = @client.user(1510554860)
+    begin
+      @profile = @client.user(params[:id])
+      @followed_by_count = @profile.counts.followed_by
+        #@profile = @client.user(1510554860)
+    rescue Instagram::BadRequest
+      @error = 'Sorry, but this account is private'
+    end
   end
 
   def limits
--- app/jobs/follow_job.rb
+++ app/jobs/follow_job.rb
@@ -1,27 +1,29 @@
-class FollowJob < ActiveJob::Base
-  queue_as :default
+class FollowJob
+  include Sidekiq::Worker
+  sidekiq_options queue: :default, retry: false
 
-  def perform(chosen_user, current_user, client, *args)
-    #options = args.extract_options!
+  def perform(chosen_user_id, current_user_id, access_token, *args)
+    options = args.extract_options!
 
-    @chosen_user = chosen_user
-    @current_user = current_user
-    @client = client
+    @client = Instagram.client client_id: ENV['INSTAGRAM_API_KEY'], client_secret: ENV['INSTAGRAM_SECRET_KEY'], client_ips: '127.0.0.1', access_token: access_token
+    @chosen_user = @client.user(chosen_user_id)
 
-    @followed_by_count = chosen_user[:counts][:followed_by]
+    @current_user = current_user_id
+
+    #@followed_by_count = chosen_user[:counts][:followed_by]
     @followed_count = 0
-
-    @next_cursor = 0
+    @next_cursor = options.cursor || 0
 
     loop do
-      @followed_by = @client.user_followed_by chosen_user[:id], next_cursor: @next_cursor
+      @followed_by = @client.user_followed_by @chosen_user[:id], next_cursor: @next_cursor
 
       @followed_by.each do |follower|
-        try
-        @client.follow_user follower.id
-
-        @followed_count += 1
-        sleep(1)
+        unless %w(follows requested).include? @client.user_relationship(follower.id).outgoing_status
+          Rails.logger.info follower
+          # @client.follow_user follower.id
+          # @followed_count += 1
+          sleep(1)
+        end
       end
 
       @next_cursor = @followed_by.pagination.next_cursor
